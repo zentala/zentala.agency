d58152229ac3b7c234343d08e200884dc8293660
Paweł Żentała <zentala@gmail.com>
2024-06-02 00:36:36 +0200

    Configure Prettier, TailwindCSS & SCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, // Włącza polling, co może pomóc w niektórych środowiskach
+      },
+      hmr: {
+        overlay: false, // Wyłącza overlay dla HMR
+      },
+    },
+  },
+})
