273d6522f763f8dd64e517eb7f932b3e7ebcba25
Paweł Żentała <zentala@gmail.com>
2024-06-02 18:10:27 +0200

    Fix errors & deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
   vite: {
     server: {
       watch: {
-        usePolling: true, // Włącza polling, co może pomóc w niektórych środowiskach
+        usePolling: true,
       },
       hmr: {
-        overlay: false, // Wyłącza overlay dla HMR
+        overlay: false,
       },
     },
   },
