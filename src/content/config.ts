7af7134eff3d0c6d6c1d863e0286148b1fa4ba8b
Paweł Żentała <zentala@gmail.com>
2024-12-13 22:27:02 +0100

    Footer and Blog design improvments

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,36 @@
-import { defineCollection } from 'astro:content'
-import { z } from 'astro/zod'
+import { defineCollection, reference, z } from 'astro:content'
+
+const authorCollection = defineCollection({
+  type: 'data',
+  schema: z.array(
+    z.object({
+      slug: z.string(),
+      name: z.string(),
+      role: z.string(),
+      avatar: z.string(),
+      profileUrl: z.string().default('/about/me'),
+      version: z.string(),
+    }),
+  ),
+})
+
+console.log('Defining author collection') // debug
 
 const blogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     date: z.string(),
     category: z.string(),
+    authorVersion: z.string(),
     imageUrl: z.string().optional(),
     excerpt: z.string().optional(),
-    authorName: z.string(),
-    authorAvatar: z.string().optional(),
-    authorRole: z.string().optional(),
+    readingTime: z.number().optional(),
+    tags: z.array(z.string()).optional(),
   }),
 })
 
 export const collections = {
   blog: blogCollection,
+  authors: authorCollection,
 }
